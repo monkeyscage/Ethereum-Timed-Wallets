4a7e0222a96f736181a54f61fd965fdd1c12aed2
maxxflyer <maxxflyer@gmail.com>
2017-04-30 11:41:35 +0200

    Update TimedWallet.sol

--- TimedWallet.sol
+++ TimedWallet.sol
@@ -1,8 +1,31 @@
-cintract TimedWallet(){
+contract TimedWallet(){
 address public owner;
+uint public blocksInterval; //the time needed to unlock
+uint public request; //the moment of the request
+address safeAddress;
+uint public amount;
 
-function TimedWallet(address o){
+function TimedWallet(address o,address safe){
 owner=o;
+safeAddress=safe;
+}
+
+function panic(){
+if(msg.sender!=owner)throw;
+if(!send(safeAddress,this.balance))throw;
+kill();
+}
+
+function RequestWithdraw(uint tot){
+if(msg.sender!=owner)throw;
+request=block.number;
+amount=tot;
+}
+
+function Withdraw(address a){
+if(msg.sender!=owner)throw;
+if(block.number<request+blocksInterval)throw;
+if(!send(a,amount))throw;
 }
 
 //change owner
