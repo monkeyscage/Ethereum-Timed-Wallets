09b7e633ca01112fab7659041a0efb853723da26
maxxflyer <maxxflyer@gmail.com>
2017-04-30 11:48:05 +0200

    Update TimedWallet.sol

--- TimedWallet.sol
+++ TimedWallet.sol
@@ -1,10 +1,10 @@
 contract TimedWallet(){
 
-address public owner;
-uint public wait; //the time needed to unlock
-uint public request; //the moment of the request
-address panicAddress; //a secondary panic address in case of problems
-uint public amount; //the amount to withdraw at the requested time
+address public owner;   //it is decided at contract creation, can't be changed
+uint public wait;       //the time needed to unlock, it is decided at contract creation, can't be changed
+address panicAddress;   //a secondary panic address in case of problems, can't be changed
+uint public request;    //the moment of the request
+uint public amount;     //the amount to withdraw at the requested time
 
 function TimedWallet(address own,address panic,uint interval){
 owner=own;
